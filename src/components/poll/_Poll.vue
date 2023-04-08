4d3a614c1313d107781864e212fe7a230d8337ff
SweckEUW <simon.weck98@yahoo.de>
2023-04-09 01:01:48 +0200

    fixed poll css

--- src/components/poll/_Poll.vue
+++ src/components/poll/_Poll.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="strawpoll-embed">
-        <iframe title="StrawPoll Embed" id="strawpoll_iframe_PbZqRY9vKyN" scrolling="no" src="https://strawpoll.com/embed/polls/PbZqRY9vKyN" frameborder="0" allowfullscreen allowtransparency>Loading...</iframe>
+        <iframe title="StrawPoll Embed" id="strawpoll_iframe_PbZqRY9vKyN" src="https://strawpoll.com/embed/polls/PbZqRY9vKyN" frameborder="0" allowfullscreen allowtransparency>Loading...</iframe>
      </div>
 </template>
 
@@ -12,12 +12,12 @@
 .strawpoll-embed{
     display: flex;
     justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100vw;
+    margin-top: 80px;
+    margin-bottom: 50px;
+    height: calc(100vh - 80px - 50px);
 }
 iframe{
-    height: 670px;
+    height: 100%;
     max-width: 640px; 
     width: 800px;
     border-radius: 10px;
@@ -27,16 +27,7 @@ iframe{
 @media (width <= 900px){
 	iframe{
         width: 90%;
-	}
-}
-@media (width <= 470px){
-	iframe{
-        height: 700px;
-	}
-}
-@media (width <= 380px){
-	iframe{
-        height: 730px;
+        height: 880px;
 	}
 }
 </style>
