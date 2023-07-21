5fce3bf0f561ff6dd0203de5202633f660349ca5
SweckEUW <simon.weck98@yahoo.de>
2023-07-21 23:05:02 +0200

    added 404 page and redirect for QR Code Sticker

---
+++ src/components/frontend/routes/_NotFound404.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="NotFound404">
+        <h1 class="bp-title" style="margin-top: 150px;">404</h1>
+        <h1 class="bp-title">Seite nicht gefunden</h1>
+    </div>
+</template>
+
+<style scoped>
+.bp-title{
+    padding-bottom: 0px;
+    padding-top: 0px;
+}
+</style>
