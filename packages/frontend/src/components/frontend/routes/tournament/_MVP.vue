33206cd715702db9d5332290503097cd46f3e967
Simon Weck <simon.weck98@yahoo.de>
2023-12-28 17:41:37 +0100

    added sorting arrows to mvp table

--- packages/frontend/src/components/frontend/routes/tournament/_MVP.vue
+++ packages/frontend/src/components/frontend/routes/tournament/_MVP.vue
@@ -10,9 +10,10 @@ const route = useRoute()
 let tournament = ref();
 let players = ref();
 let sortValue = ref("averageScore");
+let sortUp = ref(false);
+
 const getTournament = async () => {
-    // @ts-ignore 
-    tournament.value = await getTournamentWithRouterID(route.params.id);
+    tournament.value = await getTournamentWithRouterID(route.params.id as string);
     players.value = getPlayersWithStats(tournament.value);
     sortList();
 }
@@ -32,17 +33,29 @@ window.addEventListener("resize", () => {
 });
 
 const sortList = async () => {
-    // TODO
-    // players.value.sort((player1:any, player2:any) => {
-    //     if(player2[sortValue.value] == player1[sortValue.value])
-    //         return player1.placement - player2.placement
-    //     return player2[sortValue.value] - player1[sortValue.value]
-    // });
+    players.value.sort((player1:any, player2:any) => {
+        if(sortValue.value == "name") // sort strings
+            return sortUp.value ? player1[sortValue.value].localeCompare(player2[sortValue.value]) : player2[sortValue.value].localeCompare(player1[sortValue.value])
+
+        if(player2[sortValue.value] == player1[sortValue.value])
+            return sortUp.value ? player2.placement - player1.placement : player1.placement - player2.placement;
+        return sortUp.value ? player1[sortValue.value] - player2[sortValue.value] : player2[sortValue.value] - player1[sortValue.value]
+    });
 }
 
 const setSortValue = async (value:string) => {
+    if(sortValue.value == value)
+        sortUp.value = !sortUp.value;
+    else
+        sortUp.value = false;
+
     sortValue.value = value;
     sortList();
+}
+
+const giveArrowClass = (value:string) => {
+    if(windowWidth.value > 900 && value == sortValue.value)
+        return sortUp.value ? "mvp-arrow mvp-arrow-up" : "mvp-arrow mvp-arrow-down";
 }
 </script>
 
@@ -60,12 +73,12 @@ const setSortValue = async (value:string) => {
         <table v-show="tournament && players && players.length > 0" class="table table-hover caption-top" id="mvp-table">
             <thead>
                 <tr style="height: auto;">
-                    <th>{{ windowWidth > 900 ? 'Platz' :'Pl.'}}</th>
-                    <th>{{'Name'}}</th>
-                    <th @click="setSortValue('averageScore')">{{ windowWidth > 900 ? 'Trefferquote' : 'Trfq.'}}</th>
-                    <th @click="setSortValue('score')">{{ windowWidth > 900 ? 'Treffer insgesamt' : 'Trf.'}}</th>
-                    <th @click="setSortValue('ammountOfMatches')">{{ windowWidth > 900 ? 'Spiele' : 'Spi.'}}</th>
-                    <th @click="setSortValue('ammountOfDrunkenCups')">{{ windowWidth > 900 ? 'Getrunkene Becher' : 'Getru. Becher'}}</th>
+                    <th @click="setSortValue('placement')" :class="giveArrowClass('placement')">{{ windowWidth > 900 ? 'Platz' :'Pl.'}}</th>
+                    <th @click="setSortValue('name')" :class="giveArrowClass('name')">{{'Name'}}</th>
+                    <th @click="setSortValue('averageScore')" :class="giveArrowClass('averageScore')">{{ windowWidth > 900 ? 'Trefferquote' : 'Trfq.'}}</th>
+                    <th @click="setSortValue('score')" :class="giveArrowClass('score')">{{ windowWidth > 900 ? 'Treffer insgesamt' : 'Trf.'}}</th>
+                    <th @click="setSortValue('ammountOfMatches')" :class="giveArrowClass('ammountOfMatches')">{{ windowWidth > 900 ? 'Spiele' : 'Spi.'}}</th>
+                    <th @click="setSortValue('ammountOfDrunkenCups')" :class="giveArrowClass('ammountOfDrunkenCups')">{{ windowWidth > 900 ? 'Getrunkene Becher' : 'Getru. Becher'}}</th>
                 </tr>
             </thead>
             <tbody>
@@ -113,6 +126,21 @@ table th{
 table td{
     background: inherit;
 }
+.mvp-arrow-down::after, .mvp-arrow-up::after{
+    content: ' ';
+    position: relative;
+    left: 10px;
+    border: 8px solid transparent;
+}
+.mvp-arrow-down::after{
+    top: 18px;
+    border-top-color: var(--main-color);
+}
+.mvp-arrow-up::after{
+    bottom: 18px;
+    border-bottom-color: var(--main-color);
+}
+
 /*MOBILE*/
 @media (width <= 900px){
     table{
@@ -138,6 +166,17 @@ table td{
         max-width: 100px;
         width: 100px;
         white-space: break-spaces;
+    }
+
+    .mvp-arrow-down::after, .mvp-arrow-up::after{
+        left: 5px;
+        border-width: 6px;
+    }
+    .mvp-arrow-down::after{
+        top: 15px;
+    }
+    .mvp-arrow-up::after{
+        bottom: 15px;
     }
 }
 
