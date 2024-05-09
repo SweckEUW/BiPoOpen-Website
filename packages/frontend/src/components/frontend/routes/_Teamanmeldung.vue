3ff2b623ad6bd3f5cfd2899b9120e8ba8fc2c0ad
Simon Weck <Simon.Weck98@yahoo.de>
2024-05-09 14:19:17 +0200

    team registration css fixes

--- packages/frontend/src/components/frontend/routes/_Teamanmeldung.vue
+++ packages/frontend/src/components/frontend/routes/_Teamanmeldung.vue
@@ -1,14 +1,25 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+    <h1 class="bp-title">{{ "Teamanmeldung 2024" }}</h1>
    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdOD6gv1kxf9mcMggg65DbRi_zxZqq0Az0-tIGUqjoovyJWqw/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </template>
 
 <style scoped>
+iframe{
+    width: 100%;
+    height: 950px;
+}
+
+/*MOBILE*/
+@media (width <= 900px){
     iframe{
         width: 100%;
-        height: 100vh;
+        height: 1100px;
     }
+}
+/*MOBILE S*/
+@media (width <= 360px){
+    iframe{
+        height: 1200px;
+    }
+}
 </style>
