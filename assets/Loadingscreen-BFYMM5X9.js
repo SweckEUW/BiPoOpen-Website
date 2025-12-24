d75b09dcf9fa97fa1d46ec4b14909d2567a9591c
SweckEUW <SweckEUW@users.noreply.github.com>
2025-12-24 17:08:06 +0000

    deploy: dc167796e51c886d8aa0e6c29d417588fe8cf1a9

--- assets/Loadingscreen-BFYMM5X9.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as o,b as e,p as a,g as t,o as d}from"./index-Bay7AFu6.js";const n={},_=s=>(a("data-v-59b7bab0"),s=s(),t(),s),r={class:"Loadingscreen"},i=_(()=>e("div",{class:"ls-container"},[e("div",{class:"loader"})],-1)),p=[i];function l(s,b){return d(),o("div",r,p)}const f=c(n,[["render",l],["__scopeId","data-v-59b7bab0"]]);export{f as L};
